02db1b0ebaa0b567dab9dfa82a794b08f1b76c00
arslona4-blip <arslona4@gmail.com>
2026-08-08 03:26:25 +0500

    Update main.py

--- main.py
+++ main.py
@@ -73,8 +73,7 @@ async def start_handler(message: types.Message, state: FSMContext):
 
 @dp.message(F.text == "🍔 Buyurtma berish")
 async def start_order(message: types.Message, state: FSMContext):
-  if "cart" not in data:
-      await state.set_state(OrderState.choosing_products)
+    await state.set_state(OrderState.choosing_products)
     await state.update_data(cart={})
     await message.answer("Menyudan mahsulotlarni tanlang:", reply_markup=products_keyboard())
 
